b2d04564d6593b7ac6892f7f851fbb7857beedd3
Aurel Medvegy <aurel.medvegy@hotovo.org>
2014-07-25 14:27:50 +0200

    Integration test for assign dummy account.

--- src/test/java/org/mule/templates/integration/BusinessLogicTestAssignDummyAccountIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicTestAssignDummyAccountIT.java
@@ -17,7 +17,6 @@ import org.junit.Test;
 import org.mule.MessageExchangePattern;
 import org.mule.api.MuleException;
 import org.mule.api.lifecycle.InitialisationException;
-import org.mule.modules.siebel.api.model.response.CreateResult;
 import org.mule.processor.chain.InterceptingChainLifecycleWrapper;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.templates.builders.SfdcObjectBuilder;
@@ -34,10 +33,13 @@ import static org.mule.templates.builders.SfdcObjectBuilder.aContact;
  * for this Mule Anypoint Template
  */
 @SuppressWarnings("unchecked")
-public class BusinessLogicTestDoNotCreateAccountIT extends AbstractTemplateTestCase {
+public class BusinessLogicTestAssignDummyAccountIT extends AbstractTemplateTestCase {
 
+    private static final String ANYPOINT_TEMPLATE_NAME = "sfdc2sieb-contact-bidirectional-sync";
     private static final String SALESFORCE_INBOUND_FLOW_NAME = "triggerFlowToSiebel";
     private static final String SIEBEL_INBOUND_FLOW_NAME = "triggerFlowToSalesforce";
+    private static final String ACCOUNT_ID_IN_SALESFORCE = "0012000001AOHJWAA5";
+    private static final String ACCOUNT_ID_IN_SIEBEL = "1-C4QJ";
     private static final int TIMEOUT_MILLIS = 60;
 
     private static List<String> contactsCreatedInSalesforce = new ArrayList<String>();
@@ -50,11 +52,19 @@ public class BusinessLogicTestDoNotCreateAccountIT extends AbstractTemplateTestC
     private InterceptingChainLifecycleWrapper queryContactFromSalesforceFlow;
     private InterceptingChainLifecycleWrapper queryContactFromSiebelFlow;
     private BatchTestHelper batchTestHelper;
+    private SubflowInterceptingChainLifecycleWrapper createAccountInSalesforceFlow;
+    private SubflowInterceptingChainLifecycleWrapper createAccountInSiebelFlow;
+    private SubflowInterceptingChainLifecycleWrapper queryContactsAccountNameFromSalesforceFlow;
+    private SubflowInterceptingChainLifecycleWrapper queryContactsAccountNameFromSiebelFlow;
 
     @BeforeClass
     public static void beforeTestClass() {
-        System.setProperty("account.sync.policy", "");
+        // Set polling frequency to 10 seconds
         System.setProperty("poll.frequency", "10000");
+
+        System.setProperty("account.sync.policy", "assignDummyAccount");
+        System.setProperty("account.id.in.b", ACCOUNT_ID_IN_SIEBEL);
+        System.setProperty("account.id.in.a", ACCOUNT_ID_IN_SALESFORCE);
     }
 
     @Before
@@ -68,6 +78,7 @@ public class BusinessLogicTestDoNotCreateAccountIT extends AbstractTemplateTestC
     @AfterClass
     public static void shutDown() {
         System.clearProperty("poll.frequency");
+        System.clearProperty("watermark.default.expression");
         System.clearProperty("account.sync.policy");
     }
 
@@ -82,32 +93,48 @@ public class BusinessLogicTestDoNotCreateAccountIT extends AbstractTemplateTestC
     }
 
     private void getAndInitializeFlows() throws InitialisationException {
-        // Flow for creating contacts in Salesforce
+        // Flow for creating contacts in sfdc A instance
         createContactInSalesforceFlow = getSubFlow("createContactInSalesforceFlow");
         createContactInSalesforceFlow.initialise();
 
-        // Flow for creating contacts in Siebel
+        // Flow for creating contacts in sfdc B instance
         createContactInSiebelFlow = getSubFlow("createContactInSiebelFlow");
         createContactInSiebelFlow.initialise();
 
-        // Flow for deleting contacts in Salesforce
+        // Flow for deleting contacts in sfdc A instance
         deleteContactFromSalesforceFlow = getSubFlow("deleteContactFromSalesforceFlow");
         deleteContactFromSalesforceFlow.initialise();
 
-        // Flow for deleting contacts in Siebel
+        // Flow for deleting contacts in sfdc B instance
         deleteContactFromSiebelFlow = getSubFlow("deleteContactFromSiebelFlow");
         deleteContactFromSiebelFlow.initialise();
 
-        // Flow for querying contacts in Salesforce
+        // Flow for querying the contact in sfdc A instance
         queryContactFromSalesforceFlow = getSubFlow("queryContactFromSalesforceFlow");
         queryContactFromSalesforceFlow.initialise();
 
-        // Flow for querying contacts in Siebel
+        // Flow for querying the contact in sfdc B instance
         queryContactFromSiebelFlow = getSubFlow("queryContactFromSiebelFlow");
         queryContactFromSiebelFlow.initialise();
+
+        // Flow for creating accounts in sfdc A instance
+        createAccountInSalesforceFlow = getSubFlow("createAccountInSalesforceFlow");
+        createAccountInSalesforceFlow.initialise();
+
+        // Flow for creating accounts in sfdc B instance
+        createAccountInSiebelFlow = getSubFlow("createAccountInSiebelFlow");
+        createAccountInSiebelFlow.initialise();
+
+        // Flow for querying accounts in sfdc A instance
+        queryContactsAccountNameFromSalesforceFlow = getSubFlow("queryContactsAccountNameFromSalesforceFlow");
+        queryContactsAccountNameFromSalesforceFlow.initialise();
+
+        // Flow for querying accounts in sfdc B instance
+        queryContactsAccountNameFromSiebelFlow = getSubFlow("queryContactsAccountNameFromSiebelFlow");
+        queryContactsAccountNameFromSiebelFlow.initialise();
     }
 
-    private void cleanUpSandboxesByRemovingTestContacts()
+    private static void cleanUpSandboxesByRemovingTestContacts()
             throws MuleException, Exception {
 
         final List<String> idList = new ArrayList<String>();
@@ -116,77 +143,71 @@ public class BusinessLogicTestDoNotCreateAccountIT extends AbstractTemplateTestC
             idList.add(contact);
         }
 
-        if (!contactsCreatedInSalesforce.isEmpty()){
-            deleteContactFromSalesforceFlow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
-            idList.clear();
-            contactsCreatedInSalesforce.clear();
-        }
+        deleteContactFromSalesforceFlow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+        idList.clear();
 
         for (String contact : contactsCreatedInSiebel) {
             idList.add(contact);
         }
 
-        if(!contactsCreatedInSiebel.isEmpty()){
-            deleteContactFromSiebelFlow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
-            contactsCreatedInSiebel.clear();
-        }
+        deleteContactFromSiebelFlow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
     }
 
     @Test
-    public void whenUpdatingContactInSiebelTheBelongingContactGetsUpdatedInSalesforce()
-            throws MuleException, Exception {
-
-        Map<String, Object> siebelContact = createSiebelContact(1);
-
-        // Create contacts in sand-boxes and keep track of them for posterior cleaning up
-        contactsCreatedInSiebel.add(createTestContactsInSiebelSandbox(siebelContact, createContactInSiebelFlow));
-
-        // Execution
-        executeWaitAndAssertBatchJob(SIEBEL_INBOUND_FLOW_NAME);
-
-        // Assertions
-        Map<String, String> retrievedContactFromSalesforce = (Map<String, String>) querySalesforceContact(siebelContact, queryContactFromSalesforceFlow);
-        Assert.assertEquals("Some contacts are not synchronized between systems.", siebelContact.get("Email Address"), retrievedContactFromSalesforce.get("Email"));
-        // for cleaning up
-        contactsCreatedInSalesforce.add(retrievedContactFromSalesforce.get("Id"));
-}
-
-    @Test
-    public void whenUpdatingContactInSalesforceTheBelongingContactGetsUpdatedInSiebel()
+    public void whenUpsartingAContactInInstanceBTheBelongingContactGetsUpsartedInInstanceA()
             throws MuleException, Exception {
         // Build test contacts
+        String uniqueEmail = ANYPOINT_TEMPLATE_NAME + "-" + System.currentTimeMillis() + "@insalesforce.com";
         SfdcObjectBuilder contact = aContact()
-                .with("FirstName", "Peter")
-                .with("LastName", "Hobbit")
+                .with("FirstName", "Steve")
+                .with("LastName", "Smith")
                 .with("MailingCountry", "US")
-                .with("Email", "insalesforce" + "-" + System.currentTimeMillis() + "@mail.com");
+                .with("Email", uniqueEmail);
 
-        Map<String, Object> salesforceContact = contact.build();
+        Map<String, Object> salesforceContact = contact.with("AccountId", ACCOUNT_ID_IN_SALESFORCE).build();
+        Map<String, Object> salesforceContactEA = contact.with("Email Address", uniqueEmail).build();
 
-        // Create contacts in sand-boxes and keep track of them for posterior cleaning up
+        // Create contact in sand-box and keep track of it for posterior cleaning up
         contactsCreatedInSalesforce.add(createTestContactsInSfdcSandbox(salesforceContact, createContactInSalesforceFlow));
 
         // Execution
         executeWaitAndAssertBatchJob(SALESFORCE_INBOUND_FLOW_NAME);
 
+        // Assertions
+        Map<String, String> retrievedContactFromSalesforce = (Map<String, String>) querySalesforceContact(salesforceContactEA, queryContactFromSalesforceFlow);
         Map<String, String> retrievedContactFromSiebel = (Map<String, String>) querySiebelContact(salesforceContact, queryContactFromSiebelFlow);
-        Assert.assertEquals("Some contacts are not synchronized between systems.", salesforceContact.get("Email"), retrievedContactFromSiebel.get("Email Address"));
+
+        Assert.assertEquals("Email is not synchronized between systems.", salesforceContact.get("Email"), retrievedContactFromSiebel.get("Email Address"));
+        Assert.assertEquals("FirstName is not synchronized between systems.", salesforceContact.get("FirstName"), retrievedContactFromSiebel.get("First Name"));
+        Assert.assertEquals("LastName is not synchronized between systems.", salesforceContact.get("LastName"), retrievedContactFromSiebel.get("Last Name"));
         contactsCreatedInSiebel.add(retrievedContactFromSiebel.get("Id"));
+
+        Map<String, HashMap> retrievedContactsAccountIdFromSalesforce = (Map<String, HashMap>) querySalesforceContact(salesforceContact, queryContactsAccountNameFromSalesforceFlow);
+        Map<String, HashMap> retrievedContactsAccountIdFromSiebel = (Map<String, HashMap>) querySiebelContact(salesforceContact, queryContactsAccountNameFromSiebelFlow);
+
+        String salesforceAccount = String.valueOf(retrievedContactsAccountIdFromSalesforce.get("Account").get("Name"));
+        String siebelAccount = String.valueOf(retrievedContactsAccountIdFromSiebel.get("Account"));
+
+        Assert.assertEquals("Account is not synchronized between systems.", salesforceAccount, siebelAccount);
+
     }
 
     private Object querySalesforceContact(Map<String, Object> contact, InterceptingChainLifecycleWrapper queryContactFlow)
             throws MuleException, Exception {
 
-        return queryContactFlow.process(getTestEvent(contact, MessageExchangePattern.REQUEST_RESPONSE))
+        Object payload = queryContactFlow.process(getTestEvent(contact, MessageExchangePattern.REQUEST_RESPONSE))
                 .getMessage().getPayload();
+
+        return payload;
     }
 
     private Object querySiebelContact(Map<String, Object> contact, InterceptingChainLifecycleWrapper queryContactFlow)
             throws MuleException, Exception {
 
-    	List<Map<String, Object>> payload = (List<Map<String, Object>>) queryContactFlow.process(getTestEvent(contact, MessageExchangePattern.REQUEST_RESPONSE))
-				        .getMessage().getPayload();
-		return payload.isEmpty() ? new HashMap<String, Object>() : payload.get(0);
+        List<Map<String, Object>> payload = (List<Map<String, Object>>) queryContactFlow.process(getTestEvent(contact,
+                MessageExchangePattern.REQUEST_RESPONSE)).getMessage().getPayload();
+
+        return payload.isEmpty() ? new HashMap<String, Object>() : payload.get(0);
     }
 
     private String createTestContactsInSfdcSandbox(Map<String, Object> contact, InterceptingChainLifecycleWrapper createContactFlow)
@@ -202,37 +223,7 @@ public class BusinessLogicTestDoNotCreateAccountIT extends AbstractTemplateTestC
         return payloadAfterExecution.get(0).getId();
     }
 
-    private String createTestContactsInSiebelSandbox(Map<String, Object> contact, InterceptingChainLifecycleWrapper createContactFlow)
-            throws MuleException, Exception {
-
-        final CreateResult payloadAfterExecution = (CreateResult) createContactFlow
-                .process(getTestEvent(contact, MessageExchangePattern.REQUEST_RESPONSE))
-                .getMessage().getPayload();
-
-        return payloadAfterExecution.getCreatedObjects().get(0);
-    }
-
-	protected Map<String, Object> createSiebelContact(int sequence) {
-		return SfdcObjectBuilder
-				.aContact()
-				.with("First Name", "FirstName_" + sequence)
-				.with("Last Name", TEMPLATE_NAME + sequence)
-				.with("Email Address", buildUniqueEmail("insiebel." + System.currentTimeMillis())).build();
-	}
-
-	protected String buildUniqueEmail(String contact) {
-		String server = "fakemail";
-
-		StringBuilder builder = new StringBuilder();
-		builder.append( contact);
-		builder.append("@");
-		builder.append(server);
-		builder.append(".com");
-
-		return builder.toString();
-	}
-
-	private void executeWaitAndAssertBatchJob(String flowConstructName)
+    private void executeWaitAndAssertBatchJob(String flowConstructName)
             throws Exception {
 
         // Execute synchronization
